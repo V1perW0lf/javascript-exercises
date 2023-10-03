9738191514ffe3bc46f41a21da836bdc44ca77e9
CJ <11570429+V1perW0lf@users.noreply.github.com>
2023-10-03 12:25:14 -0500

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let stringToCheck = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s/g,"").toLowerCase();
+    let stringArray = stringToCheck.split("");
+    let reversedArray = stringArray.reverse();
+    let reversedString = reversedArray.join("");
+    if(reversedString == stringToCheck) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
