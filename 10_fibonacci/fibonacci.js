0b7d312bea77e606cf13439fa82bf7d944328817
CJ <11570429+V1perW0lf@users.noreply.github.com>
2023-10-03 18:13:22 -0500

    solve

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let firstPrevious = 1;
+    let secondPrevious = 0;
+    let current = 0;
+    for(let i = 1; i < +num; i++) {
+        current = firstPrevious + secondPrevious;
+        secondPrevious = firstPrevious;
+        firstPrevious = current;
+    }
+    return num > 0 ? firstPrevious : "OOPS";
 };
 
 // Do not edit below this line
